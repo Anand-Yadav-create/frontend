ed467b4364dd6ba7a3e5ced541196a8e5137d1f4
Anand-Yadav-create <anandprakashhacker@gmail.com>
2025-06-08 16:41:56 +0530

    hel

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 
-import dotenv from "dotenv";
-dotenv.config();
+
 const firebaseConfig = {
-  apiKey: process.env.YOUR_API_KEY,
-  authDomain: process.env.your-app.firebaseapp.com,
-  projectId: process.env.YOUR_PROJECT_ID,
-  storageBucket: process.env.your-app.appspot.com,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
